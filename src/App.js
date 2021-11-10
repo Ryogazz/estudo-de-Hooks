c71c490ac23c66b75693944006b08e8333cf11c7
Ryogazz <josuex.x.x@hotmail.com>
2021-11-09 21:17:27 -0300

    Hooks usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0) // seta o estado do nosso contador como 0
+
+  // funçao que vai setar o novo valor para contador
+  const clicador = () =>{
+    setContador((contador) => contador + 1); //Boa pratica usar uma funçao assim. com callback
+  };
+
+  const clicadorNegativo = () =>{
+    setContador((contador) => contador - 1);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hooks</h1>
+    <h2>contador{ contador }</h2>
+    <p>
+      <button type='button' onClick={clicador}>
+      contador { contador }
+      </button>
+    </p>
+
+    <p>
+      <button type='button' onClick={clicadorNegativo}>
+      contador - { contador }
+      </button>
+    </p>
+
     </div>
   );
 }
